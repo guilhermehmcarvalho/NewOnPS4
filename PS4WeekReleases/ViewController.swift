f50f61ef16e68786fed3ffd6d047e4f3beccd037
Guilherme <guilhermehmcarvalho@gmail.com>
2018-05-20 11:01:32 +0200

    [Added] Xcode project

---
+++ PS4WeekReleases/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PS4WeekReleases
+//
+//  Created by Guilherme Carvalho on 20/05/2018.
+//  Copyright © 2018 gcarvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
