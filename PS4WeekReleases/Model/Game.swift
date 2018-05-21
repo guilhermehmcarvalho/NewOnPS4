0e74735d8adaf9dbb5d30d76bb6123699b525bde
Guilherme <guilhermehmcarvalho@gmail.com>
2018-05-21 08:01:16 +0200

    [Added] Models

---
+++ PS4WeekReleases/Model/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  PS4WeekReleases
+//
+//  Created by Guilherme Carvalho on 20/05/2018.
+//  Copyright © 2018 gcarvalho. All rights reserved.
+//
+
+import Foundation
+
+struct Game: Decodable {
+    
+    let id: Int
+    let name: String
+    let summary: String
+    
+}
---
+++ PS4WeekReleases/Model/ReleaseDate.swift
@@ -0,0 +1,15 @@
+//
+//  ReleaseDate.swift
+//  PS4WeekReleases
+//
+//  Created by Guilherme Carvalho on 20/05/2018.
+//  Copyright © 2018 gcarvalho. All rights reserved.
+//
+
+import Foundation
+
+struct ReleaseDate: Decodable {
+    let id: String
+    let game: Game
+    let date: u_long
+}
