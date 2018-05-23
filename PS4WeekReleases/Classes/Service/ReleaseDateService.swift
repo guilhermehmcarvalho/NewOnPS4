693130fd343f3077a3a42f847216147238252ed7
vao <developer@vao.pl>
2018-05-23 08:54:28 +0200

    [Added] ReleaseDateService

---
+++ PS4WeekReleases/Classes/Service/ReleaseDateService.swift
@@ -0,0 +1,50 @@
+//
+//  ReleaseDateService.swift
+//  PS4WeekReleases
+//
+//  Created by Guilherme on 22/05/2018.
+//  Copyright © 2018 gcarvalho. All rights reserved.
+//
+
+import Foundation
+
+class ReleaseDateService: Service<ReleaseDate> {
+	
+	// MARK: - Variable
+	
+	private let apiService = ReleaseDateApiService()
+	public var delegate:ReleaseDateServiceDelegate?
+	
+	// MARK: - Public
+	
+	func getPlaystationWeek() {
+		apiService.get(timestamp: Date().millisecondsSince1970,
+				 platform: 48,
+				 order: "date:asc",
+				 success: success, failure: failure)
+	}
+	
+	// MARK: - Private
+	
+	private func success(data: Data) {
+		DispatchQueue.main.async {
+			if let releaseDates = self.jsonDecodeArray(data: data) {
+				self.delegate?.getPlaystationWeekDidComplete(releaseDates: releaseDates)
+			} else {
+				self.delegate?.getPlaystationWeekDidComplete(failure: .server)
+			}
+		}
+	}
+	
+	private func failure(_ failure: ServiceFailureType) {
+		DispatchQueue.main.async {
+			self.delegate?.getPlaystationWeekDidComplete(failure: failure)
+		}
+	}
+	
+}
+
+protocol ReleaseDateServiceDelegate: class {
+	func getPlaystationWeekDidComplete(releaseDates: [ReleaseDate])
+	func getPlaystationWeekDidComplete(failure: ServiceFailureType)
+}
