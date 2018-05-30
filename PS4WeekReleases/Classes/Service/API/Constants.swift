1c8cf4e22e4a1c7969284e1c2e5722101d11126f
guilhermehmcarvalho <guilhermehmcarvalho@gmail.com>
2018-05-30 12:54:47 +0200

    Added constant file with API key

---
+++ PS4WeekReleases/Classes/Service/API/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  PS4WeekReleases
+//
+//  Created by Guilherme on 22/05/2018.
+//  Copyright © 2018 gcarvalho. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+	internal static let APIKey = "41eb34b6c4b919eb3a83baa38fe188b3"
+}
